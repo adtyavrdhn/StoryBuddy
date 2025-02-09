149a35af75dd3cd52d844e86a8714d33f4c37f4d
adtyavrdhn <adtyavrdhn@gmail.com>
2025-02-09 21:12:35 +0530

    Setting up postgres

---
+++ StoryPanda/apps/users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class User(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Username: {self.username} - Email: {self.email}"
+
+    def __repr__(self):
+        return f"{self.username} - {self.email}"
